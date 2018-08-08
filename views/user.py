71565c0bbef796199091d2040a06a151cbadc2b7
trustmub@gmail.com <trustmub@gmail.com>
2018-08-08 16:50:45 +0200

    initial commit

--- views/user.py
+++ views/user.py
@@ -2,6 +2,7 @@ import os
 from flask import Blueprint, render_template, redirect, request, url_for, flash
 from flask_bcrypt import Bcrypt
 from werkzeug.utils import secure_filename
+from forms.registration import RegistrationsForm
 
 from utilities.verifier import Verify
 from functions.genarators import *
